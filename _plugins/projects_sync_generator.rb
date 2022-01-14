e6cf1dd0617417b213baa1ed764e69deffe8edcf
qichunren <whyruby@gmail.com>
2022-01-14 13:38:40 +0800

    Cache request

--- _plugins/projects_sync_generator.rb
+++ _plugins/projects_sync_generator.rb
@@ -6,26 +6,58 @@ module RailsProjectStats
       
       def generate(site)
         projects_data = []
+        need_dump = false
         site.data["project_urls"].each do |project_url_item|
             project_meta = {}
-            project_url = project_url_item["url"]    
+            project_url = project_url_item["url"]
+            project_id = project_url_item["id"].to_i
             if project_url.start_with?("https://github.com/")
                 project_url.sub!("https://github.com/", "https://api.github.com/repos/")    
             end
-            puts "Sync project #{project_url}"
-            begin
+            need_sync_repo = false
+            response_data = nil
+            if project_id > 0
+              cache_repo_json = File.expand_path("../../_data/repos/#{project_id}.json")
+              if File.exist?(cache_repo_json)
+                if Time.now - File.mtime(cache_repo_json) > 86400 # 60 * 60 * 24
+                  need_sync_repo = true
+                else
+                  response_data = File.read(cache_repo_json)
+                  json_data = JSON.parse(response_data) rescue {}
+                end
+              else
+                need_sync_repo = true
+              end
+            else
+              need_sync_repo = true
+              need_dump = true
+            end
+
+            if need_sync_repo
+              puts "Sync project #{project_url}"
+              begin
                 response_data = URI.open(project_url).read
-            rescue OpenURI::HTTPError
+              rescue OpenURI::HTTPError
                 puts "Got error:" + $!.message
                 next
+              end
+              json_data = JSON.parse(response_data) rescue {}
+              repo_id = json_data["id"]
+              File.open(File.expand_path("../../_data/repos/#{repo_id}.json", __FILE__), "w") do |f|
+                f.write response_data
+              end
             end
+
             #puts response_data
-            json_data = JSON.parse(response_data) rescue {}
-            project_meta["name"] = json_data["name"]
+            project_meta["id"] = json_data["id"]
+            project_meta["name"] = json_data["full_name"]
             project_meta["url"] = json_data["clone_url"]
             project_meta["pushed_at"] = json_data["pushed_at"]
             project_meta["stargazers_count"] = json_data["stargazers_count"]
             projects_data << project_meta
+        end
+        if need_dump
+          File.open(File.expand_path("../../_data/project_urls.yml", __FILE__), 'w') {|f| f.write projects_data.to_yaml } rescue nil
         end
         puts "Info:" + projects_data.inspect
         stats_tpl = site.pages.find { |page| page.name == 'stats.html'}
